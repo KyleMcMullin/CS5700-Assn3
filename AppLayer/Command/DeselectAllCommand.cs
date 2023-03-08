84443b9679c8d4b01b463f6c9d08eb3043dea31e
KyleMcMullin <mcmullinjkyle@gmail.com>
2023-03-08 14:41:39 -0700

    initial from forests

---
+++ AppLayer/Command/DeselectAllCommand.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AppLayer.DrawingComponents;
+
+namespace AppLayer.Command
+{
+    public class DeselectAllCommand : Command
+    {
+        private List<Element> _selectedElements; 
+        internal DeselectAllCommand() { }
+
+        public override bool Execute()
+        {
+            _selectedElements =  TargetDrawing?.DeselectAll();
+            return _selectedElements != null && _selectedElements.Count > 0;
+        }
+
+        internal override void Undo()
+        {
+            if (_selectedElements == null || _selectedElements.Count == 0) return;
+
+            foreach (var element in _selectedElements.Where(element => !element.IsSelected))
+            {
+                element.IsSelected = true;
+                TargetDrawing.IsDirty = true;
+            }
+        }
+
+        internal override void Redo()
+        {
+            if (_selectedElements == null || _selectedElements.Count == 0) return;
+
+            foreach (var element in _selectedElements.Where(element => element.IsSelected))
+            {
+                element.IsSelected = false;
+                TargetDrawing.IsDirty = true;
+            }
+        }
+    }
+}
